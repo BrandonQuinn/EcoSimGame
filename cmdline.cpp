b5a728c12ac38ef0fe423595e83a3f6d32eaafc7
BrandonQuinn <1627916+BrandonQuinn@users.noreply.github.com>
2023-09-25 22:26:31 +1000

    more cmd stuff

--- cmdline.cpp
+++ cmdline.cpp
@@ -1,10 +1,42 @@
 #include "cmdline.h"
+#include "item.h"
+#include <iostream>
+
+using namespace std;
+
+// get the item information from the command line from the user
+item readitem() {
+    // name, manufacturing cost, life (days), chance of failure (pd)
+    string name;
+    int manufactureCost;
+    int lifeDays;
+    float failureChance;
+    item item;
+    
+    cout << " == New Item == " << endl;
+    cout << "Enter name: ";
+    cin >> name;
+
+    cout << "Enter manufacturing cost (full number): ";
+    cin >> manufactureCost;
+
+    cout << "Enter life (in days): ";
+    cin >> lifeDays;
+
+    cout << "Enter failure change (decimal percentage e.g. 0.23): ";
+    cin >> failureChance;
+
+    // TODO: Finish creating item and validating input
+
+    return item;
+}
 
 int interpretCmd(string cmd) {
     if (cmd == "exit") {
         return -1;
-    } else if (cmd == "new item") {
-        
+    } else if (cmd == "new item") {  
+        item i = readitem();
+
     } else {
         return 1;
     }
